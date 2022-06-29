f3349f52e9b68d8a46aa28f8293f0c6a031cfd93
etern <eternaldream799@gmail.com>
2022-06-30 00:51:58 +0600

    changes done

--- src/main/java/com/mjc/stage0/FirstApplication.java
+++ src/main/java/com/mjc/stage0/FirstApplication.java
@@ -2,7 +2,6 @@ package com.mjc.stage0;
 
 public class FirstApplication {
     public static void main(String[] args) {
-        String hello = "Hello world";
-        System.out.println(hello);
+        System.out.println("Hello World!");
     }
 }
